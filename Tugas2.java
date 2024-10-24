fc342710b26052fa0a1aa89c3f1238fc8db7b17c
FebryanAkt <febryan08022006@gmail.com>
2024-10-24 18:28:21 +0700

    Tugas 2

---
+++ Tugas2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Tugas2 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String nama, cabor;
+
+        System.out.print("Masukkan cabang olahraga: ");
+        cabor = sc.nextLine();
+        for (int i = 1; i<=5; i++) {
+            System.out.print("Nama atlet " +i + ": ");
+            nama = sc.nextLine();
+        }
+        System.out.print("Masukkan cabang olahraga: ");
+        cabor = sc.nextLine();
+        for (int i = 1; i<=5; i++) {
+            System.out.print("Nama atlet " +i + ": ");
+            nama = sc.nextLine();
+        }
+        System.out.print("Masukkan cabang olahraga: ");
+        cabor = sc.nextLine();
+        for (int i = 1; i<=5; i++) {
+            System.out.print("Nama atlet " +i + ": ");
+            nama = sc.nextLine();
+        }
+        System.out.print("Masukkan cabang olahraga: ");
+        cabor = sc.nextLine();
+        for (int i = 1; i<=5; i++) {
+            System.out.print("Nama atlet " +i + ": ");
+            nama = sc.nextLine();
+        }
+    }
+}
